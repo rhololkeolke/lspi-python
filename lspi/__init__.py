522e701f96f44cf2825ce9da541671a5b794ad8f
Devin Schwab <digidevin@gmail.com>
2015-03-19 03:23:14 -0400

    Added lspi package

---
+++ lspi/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""Least Squares Policy Iteration (LSPI) implementation.
+
+Implements the algorithms described in the paper
+
+"Least-Squares Policy Iteration."
+Lagoudakis, Michail G., and Ronald Parr.
+Journal of Machine Learning Research 4, 2003.
+https://www.cs.duke.edu/research/AI/LSPI/jmlr03.pdf
+
+The implementation is based on the Matlab implementation provided by
+the authors. The implementation is available for download at
+http://www.cs.duke.edu/research/AI/LSPI/
+
+"""
