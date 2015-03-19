79238b5ca2059f401134f003d2baf7cf4dd1772a
Devin Schwab <digidevin@gmail.com>
2015-03-19 17:36:54 -0400

    Added OneDimensionalPolynomialBasis class and associated tests

--- lspi/basis_functions.py
+++ lspi/basis_functions.py
@@ -2,6 +2,7 @@
 """Abstract Base Class for Basis Function and some common implementations."""
 
 import abc
+import numpy as np
 
 
 class BasisFunction(object):
@@ -10,14 +11,15 @@ class BasisFunction(object):
 
     A basis function is a function that takes in a state vector and an action
     index and returns a vector of features. The resulting feature vector is
-    referred to as Phi in the LSPI paper (pg 9 of the PDF referenced in this
-    package's documentation). The Phi vector is dotted with the weight vector
-    of the Policy to calculate the Q-value.
+    referred to as :math:`\phi` in the LSPI paper (pg 9 of the PDF referenced
+    in this package's documentation). The :math:`\phi` vector is dotted with
+    the weight vector of the Policy to calculate the Q-value.
 
     The dimensions of the state vector are usually smaller than the dimensions
-    of the Phi vector. However, the dimensions of the Phi vector are usually
-    much smaller than the dimensions of an exact representation of the state
-    which leads to significant savings when computing and storing a policy.
+    of the :math:`\phi` vector. However, the dimensions of the :math:`\phi`
+    vector are usually much smaller than the dimensions of an exact
+    representation of the state which leads to significant savings when
+    computing and storing a policy.
 
     """
 
@@ -30,14 +32,15 @@ class BasisFunction(object):
         Returns
         -------
         int
-            The size of the Phi vector. (Referred to as k in the paper).
+            The size of the :math:`\phi` vector.
+            (Referred to as k in the paper).
 
         """
         pass  # pragma: no cover
 
     @abc.abstractmethod
     def evaluate(self, state, action):
-        """Calculate the Phi matrix for the given state-action pair.
+        """Calculate the :math:`\phi` matrix for the given state-action pair.
 
         The way this value is calculated depends entirely on the concrete
         implementation of BasisFunction.
@@ -55,7 +58,112 @@ class BasisFunction(object):
         Returns
         -------
         numpy.array
-            The Phi vector. Used by Policy to compute Q-value.
+            The :math:`\phi` vector. Used by Policy to compute Q-value.
 
         """
         pass  # pragma: no cover
+
+
+class OneDimensionalPolynomialBasis(BasisFunction):
+    """Polynomial features for a state with one dimension.
+
+    Takes the value of the state and constructs a vector proportional
+    to the specified degree and number of actions. The polynomial is first
+    constructed as [..., 1, value, value^2, ..., value^k, ...]
+    where k is the degree. The rest of the vector is 0.
+
+    Parameters
+    ----------
+    degree : int
+        The polynomial degree.
+    num_actions: int
+        The total number of possible actions
+
+    """
+
+    def __init__(self, degree, num_actions):
+        if degree < 0:
+            raise ValueError('Degree must be >= 0')
+        self.degree = degree
+
+        if num_actions < 1:
+            raise ValueError('There must be at least 1 action')
+        self.num_actions = num_actions
+
+    def size(self):
+        """Calculate the size of the basis function.
+
+        The base size will be degree + 1. This basic matrix is then
+        duplicated once for every action. Therefore the size is equal to
+        (degree + 1) * number of actions
+
+
+        Returns
+        -------
+        int
+            The size of the phi matrix that will be returned from evaluate.
+
+
+        Example
+        -------
+
+        >>> basis = OneDimensionalPolynomialBasis(2, 2)
+        >>> basis.size()
+        6
+
+        """
+        return (self.degree + 1) * self.num_actions
+
+    def evaluate(self, state, action):
+        """Calculate :math:`\phi` matrix for given state action pair
+
+        The :math:`\phi` matrix is used to calculate the Q function for the
+        given policy.
+
+        Parameters
+        ----------
+        state : numpy.array
+            The state to get the features for.
+            When calculating Q(s, a) this is the s.
+        action : int
+            The action index to get the features for.
+            When calculating Q(s, a) this is the a.
+
+        Returns
+        -------
+        numpy.array
+            The :math:`\phi` vector. Used by Policy to compute Q-value.
+
+        Raises
+        ------
+        IndexError
+            If :math:`0 \le action < num\_actions` then IndexError is raised.
+        ValueError
+            If the state vector has any number of dimensions other than 1 a
+            ValueError is raised.
+
+        Example
+        -------
+
+        >>> basis = OneDimensionalPolynomialBasis(2, 2)
+        >>> basis.evaluate(np.array([2]), 0)
+        array([ 1.,  2.,  4.,  0.,  0.,  0.])
+
+        """
+
+        if action < 0 or action >= self.num_actions:
+            raise IndexError("Action index out of bounds")
+
+        if state.shape != (1, ):
+            raise ValueError("This class only supports one dimensional states")
+
+        phi = np.zeros((self.size(), ))
+
+        offset = self.size()*action
+
+        value = state[0]
+
+        phi[offset:offset + self.degree + 1] = \
+            np.array([pow(value, i) for i in range(self.degree+1)])
+
+        return phi
--- lspi_testsuite/test_basis_functions.py
+++ lspi_testsuite/test_basis_functions.py
@@ -2,8 +2,8 @@
 """Contains unit tests for the basis function module."""
 from unittest import TestCase
 
-from lspi.basis_functions import BasisFunction
-
+from lspi.basis_functions import BasisFunction, OneDimensionalPolynomialBasis
+import numpy as np
 
 class TestBasisFunction(TestCase):
     def test_require_size_method(self):
@@ -37,3 +37,53 @@ class TestBasisFunction(TestCase):
                 pass
 
         ShouldWorkBasis()
+
+class TestOneDimensionalPolynomialBasis(TestCase):
+    def setUp(self):
+
+        self.basis = OneDimensionalPolynomialBasis(2, 2)
+
+    def test_specify_degree(self):
+
+        self.assertEqual(self.basis.degree, 2)
+
+    def test_specify_actions(self):
+
+        self.assertEqual(self.basis.num_actions, 2)
+
+    def test_out_of_bounds_degree(self):
+
+        with self.assertRaises(ValueError):
+            OneDimensionalPolynomialBasis(-1, 2)
+
+    def test_out_of_bounds_num_action(self):
+
+        with self.assertRaises(ValueError):
+            OneDimensionalPolynomialBasis(2, 0)
+
+        with self.assertRaises(ValueError):
+            OneDimensionalPolynomialBasis(2, -1)
+
+    def test_size(self):
+
+        self.assertEqual(self.basis.size(), 6)
+
+    def test_evaluate(self):
+
+        phi = self.basis.evaluate(np.array([2]), 0)
+        self.assertEqual(phi.shape, (6, ))
+        np.testing.assert_array_almost_equal(phi,
+                                             np.array([1., 2., 4., 0., 0., 0.]))
+
+    def test_evaluate_out_of_bounds_action(self):
+
+        with self.assertRaises(IndexError):
+            self.basis.evaluate(np.array([2]), 2)
+
+        with self.assertRaises(IndexError):
+            self.basis.evaluate(np.array([2]), -1)
+
+    def test_evaluate_incorrect_state_dimensions(self):
+
+        with self.assertRaises(ValueError):
+            self.basis.evaluate(np.array([2, 3]), 0)
