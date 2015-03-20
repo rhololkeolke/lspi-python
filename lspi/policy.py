56ce140e838656a288241aa21a9802d3162f62e6
Devin Schwab <digidevin@gmail.com>
2015-03-20 00:46:09 -0400

    Added Policy __init__ and copy method with associated tests.

---
+++ lspi/policy.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""LSPI Policy class used for learning and executing policy."""
+
+import numpy as np
+
+
+class Policy(object):
+
+    r"""Represents LSPI policy. Used for sampling, learning, and executing.
+
+    The policy class includes an exploration value which controls the
+    probability of performing a random action instead of the best action
+    according to the policy. This can be useful during sample.
+
+    It also includes the discount factor :math:`\gamma`, number of possible
+    actions and the basis function used for this policy.
+
+    Parameters
+    ----------
+    basis: BasisFunction
+        The basis function used to compute :math:`phi` which is used to select
+        the best action according to the policy
+    discount: float, optional
+        The discount factor :math:`\gamma`. Defaults to 1.0 which is valid
+        for finite horizon problems.
+    explore: float, optional
+        Probability of executing a random action instead of the best action
+        according to the policy. Defaults to 0 which is no exploration.
+    weights: numpy.array or None
+        The weight vector which is dotted with the :math:`\phi` vector from
+        basis to produce the approximate Q value. When None is passed in
+        the weight vector is initialized with random weights.
+
+    Raises
+    ------
+    ValueError
+        If discount is < 0 or > 1
+    ValueError
+        If explore is < 0 or > 1
+    ValueError
+        If weights are not None and the number of dimensions does not match
+        the size of the basis function.
+    """
+
+    def __init__(self, basis, discount=1.0,
+                 explore=0.0, weights=None):
+        """Initialize a Policy."""
+        self.basis = basis
+
+        if discount < 0.0 or discount > 1.0:
+            raise ValueError('discount must be in range [0, 1]')
+
+        self.discount = discount
+
+        if explore < 0.0 or explore > 1.0:
+            raise ValueError('explore must be in range [0, 1]')
+
+        self.explore = explore
+
+        if weights is None:
+            self.weights = np.random.uniform(-1.0, 1.0, size=(basis.size(),))
+        else:
+            if weights.shape != (basis.size(), ):
+                raise ValueError('weights shape must equal (basis.size(), 1)')
+            self.weights = weights
+
+    def __copy__(self):
+        """Return a copy of this class with a deep copy of the weights."""
+        return Policy(self.basis,
+                      self.discount,
+                      self.explore,
+                      self.weights.copy())
---
+++ lspi_testsuite/test_policy.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+from unittest import TestCase
+
+from lspi.policy import Policy
+from lspi.basis_functions import FakeBasis
+import numpy as np
+from copy import copy
+
+class TestPolicy(TestCase):
+
+    def create_policy(self, *args, **kwargs):
+        return Policy(FakeBasis(5), *args, **kwargs)
+
+    def test_default_constructor(self):
+        policy = self.create_policy()
+
+        self.assertTrue(isinstance(policy.basis, FakeBasis))
+        self.assertAlmostEqual(policy.discount, 1.0)
+        self.assertAlmostEqual(policy.explore, 0.0)
+        self.assertEqual(policy.weights.shape, (1,))
+
+    def test_full_constructor(self):
+        policy = self.create_policy(.5, .1, np.array([1.]))
+
+        self.assertTrue(isinstance(policy.basis, FakeBasis))
+        self.assertAlmostEqual(policy.discount, .5)
+        self.assertAlmostEqual(policy.explore, 0.1)
+        np.testing.assert_array_almost_equal(policy.weights, np.array([1.]))
+
+    def test_discount_out_of_bounds(self):
+        with self.assertRaises(ValueError):
+            self.create_policy(discount=-1.0)
+
+        with self.assertRaises(ValueError):
+            self.create_policy(discount=1.1)
+
+    def test_explore_out_of_bounds(self):
+        with self.assertRaises(ValueError):
+            self.create_policy(explore=-.01)
+
+        with self.assertRaises(ValueError):
+            self.create_policy(explore=1.1)
+
+    def test_weight_basis_dimensions_mismatch(self):
+        with self.assertRaises(ValueError):
+            self.create_policy(weights=np.arange(2))
+
+    def test_copy(self):
+        orig_policy = self.create_policy()
+        policy_copy = copy(orig_policy)
+
+        self.assertNotEqual(id(orig_policy), id(policy_copy))
+        self.assertEqual(orig_policy.basis.num_actions,
+                         policy_copy.basis.num_actions)
+        self.assertEqual(orig_policy.discount, policy_copy.discount)
+        self.assertEqual(orig_policy.explore, policy_copy.explore)
+        np.testing.assert_array_almost_equal(orig_policy.weights,
+                                             policy_copy.weights)
+
+        self.assertNotEqual(id(orig_policy.weights), id(policy_copy.weights))
+
+        # verify that changing a weight in the original doesn't affect the copy
+        orig_policy.weights[0] *= -1
+
+        # numpy doesn't have an assert if not equal method
+        # so to do the inverse I'm asserting the two arrays are equal
+        # and expecting the assertion to fail
+        with self.assertRaises(AssertionError):
+            np.testing.assert_array_almost_equal(orig_policy.weights,
+                                                 policy_copy.weights)
