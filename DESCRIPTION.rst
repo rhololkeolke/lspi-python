aee8a20ff5f79d538a8f87d7027248127ec3f6ed
Devin Schwab <digidevin@gmail.com>
2015-05-24 20:08:53 -0400

    Created setup.py and DESCRIPTION.rst file.

---
+++ DESCRIPTION.rst
@@ -0,0 +1,14 @@
+LSPI Python
+===========
+
+This is a Python implementation of the Least Squares Policy Iteration (LSPI) reinforcement learning algorithm.
+For more information on the algorithm please refer to the paper
+
+| “Least-Squares Policy Iteration.”
+| Lagoudakis, Michail G., and Ronald Parr.
+| Journal of Machine Learning Research 4, 2003.
+| `<https://www.cs.duke.edu/research/AI/LSPI/jmlr03.pdf>`_
+
+You can also visit their website where more information and a Matlab version is provided.
+
+`<http://www.cs.duke.edu/research/AI/LSPI/>`_
---
+++ setup.py
@@ -0,0 +1,33 @@
+"""A setuptools based setup module."""
+
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='lspi-python',
+    version='1.0.0',
+    description='LSPI algorithm in Python',
+    long_description=long_description,
+    url='https://github.com/rhololkeolke/lspi-python',
+    author='Devin Schwab',
+    author_email='digidevin@gmail.com',
+    license='BSD-3-Clause',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.7'
+    ],
+    keywords='machinelearning ai',
+    packages=find_packages(exclude=['docs', '*testsuite', 'test*']),
+    install_requires=['numpy', 'scipy'],
+    extras_require={
+        'test': ['nosetests', 'coverage']
+    }
+)
